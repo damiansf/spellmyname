0f92d67500cfedbad74e69d626572a45e7a48eda
damiansf <damiansandhu11@gmail.com>
2019-05-28 22:22:28 -0400

    changing tabbing

--- spellMyName.py
+++ spellMyName.py
@@ -1,16 +1,16 @@
 from pygame import mixer
 
 def checkSpelling(userInput):
-	if userInput == "damian sandhu-franceschi":
-		print("You win!!! we don't give out prizes though... sooooo please leave")
-		quitGame()
-	else:
-		print("So close!!! (Just kidding... or am I)")
+    if userInput == "damian sandhu-franceschi":
+        print("You win!!! we don't give out prizes though... sooooo please leave")
+        quitGame()
+    else:
+        print("So close!!! (Just kidding... or am I)")
 
 def playAudio(times):
-	mixer.music.play(times, 0)
+    mixer.music.play(times, 0)
 def quitGame():
-	exit()
+    exit()
 
 
 print("Welcome to spell my name!\nA game that came from a joke and years of people spelling my name wrong\n\n")
@@ -21,11 +21,11 @@ mixer.music.load("myname.mp3")
 playAudio(2)
 
 while userInput.lower() != 'q':
-	userInput = str(input(("Enter L to hear my name again, Q if you want to quit or type a guess and hit enter:\n"))).lower()
-	
-	if userInput == 'q':
-		quitGame()
-	elif userInput == 'l':
-		playAudio(1)
-	else:
-		checkSpelling(userInput)    
+    userInput = str(input(("Enter L to hear my name again, Q if you want to quit or type a guess and hit enter:\n"))).lower()
+    
+    if userInput == 'q':
+        quitGame()
+    elif userInput == 'l':
+        playAudio(1)
+    else:
+        checkSpelling(userInput)    
